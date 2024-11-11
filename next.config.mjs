7245ff9ae7b5338de913dcbb6c546210d31ab1db
Roman Denysov <romandenysovsk@gmail.com>
2024-11-12 00:17:57 +0100

    main/roman/fixed-auth-1

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'standalone',
+  output: 'standalone',
 
   images: {
     minimumCacheTTL: 31536000,
--- src/server/payload/payload.config.ts
+++ src/server/payload/payload.config.ts
@@ -1,72 +1,75 @@
 import path from 'node:path'
-import {fileURLToPath} from 'node:url'
+import { fileURLToPath } from 'node:url'
 // storage-adapter-import-placeholder
-import {postgresAdapter} from '@payloadcms/db-postgres'
-import {payloadCloudPlugin} from '@payloadcms/plugin-cloud'
-import {lexicalEditor} from '@payloadcms/richtext-lexical'
-import {buildConfig} from 'payload'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { payloadCloudPlugin } from '@payloadcms/plugin-cloud'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { buildConfig } from 'payload'
 import sharp from 'sharp'
 import {
-	Carts,
-	Categories,
-	Comments,
-	Likes,
-	Media,
-	Orders,
-	Posts,
-	Products,
-	Profiles,
-	Stores,
-	Supports,
-	Tags,
-	Users
+  Carts,
+  Categories,
+  Comments,
+  Likes,
+  Media,
+  Orders,
+  Posts,
+  Products,
+  Profiles,
+  Stores,
+  Supports,
+  Tags,
+  Users,
 } from './collections'
 
-import {authjsPlugin} from 'payload-authjs'
-import {env} from '~/env'
+import { authjsPlugin } from 'payload-authjs'
+import { env } from '~/env'
 import authConfig from '~/lib/auth/auth.config'
-import {COLLECTIONS} from './config'
+import { COLLECTIONS } from './config'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
-	admin: {
-		user: COLLECTIONS.USERS,
-		importMap: {
-			baseDir: path.resolve(dirname)
-		}
-	},
-	collections: [
-		Users,
-		Products,
-		Orders,
-		Carts,
-		Likes,
-		Media,
-		Posts,
-		Comments,
-		Profiles,
-		Supports,
-		Tags,
-		Categories,
-		Stores
-	],
-	editor: lexicalEditor(),
-	secret: env.PAYLOAD_SECRET,
-	cors: ['https://checkout.stripe.com', env.NEXT_PUBLIC_SERVER_URL],
-	csrf: ['https://checkout.stripe.com', env.NEXT_PUBLIC_SERVER_URL],
-	typescript: {
-		outputFile: path.resolve(dirname, 'payload-types.ts'),
-		autoGenerate: true
-	},
-	db: postgresAdapter({
-		pool: {
-			connectionString: env.DATABASE_URL
-		},
-		push: true,
-		idType: 'uuid'
-	}),
-	sharp,
-	plugins: [authjsPlugin({authjsConfig: authConfig}), payloadCloudPlugin()]
+  admin: {
+    user: COLLECTIONS.USERS,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  collections: [
+    Users,
+    Products,
+    Orders,
+    Carts,
+    Likes,
+    Media,
+    Posts,
+    Comments,
+    Profiles,
+    Supports,
+    Tags,
+    Categories,
+    Stores,
+  ],
+  editor: lexicalEditor(),
+  secret: env.PAYLOAD_SECRET,
+  cors: ['https://checkout.stripe.com', env.NEXT_PUBLIC_SERVER_URL],
+  csrf: ['https://checkout.stripe.com', env.NEXT_PUBLIC_SERVER_URL],
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+    autoGenerate: true,
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: env.DATABASE_URL,
+    },
+    push: true,
+    idType: 'uuid',
+  }),
+  graphQL: {
+    disable: true,
+  },
+  sharp,
+  plugins: [authjsPlugin({ authjsConfig: authConfig }), payloadCloudPlugin()],
 })
